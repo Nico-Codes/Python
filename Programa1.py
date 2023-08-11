3a80fd140e8c97cbb018c05eb493e9e10caf5dc9
Nico-Codes <nicolas5905code@gmail.com>
2023-08-11 11:10:14 -0300

    Programa 11/8

--- Programa1.py
+++ Programa1.py
@@ -1,46 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-urls = [
-    'https://www.evophone.com.ar/categoria-producto/modulos/page/1/',
-    'https://www.evophone.com.ar/categoria-producto/modulos/page/2/',
-    'https://www.evophone.com.ar/categoria-producto/modulos/page/3/',
-    'https://www.evophone.com.ar/categoria-producto/modulos/page/4/ '] 
-
-print("Módulos disponibles:")
-#Primer Proceso
-for url in urls:
+def process_page(url):
     response = requests.get(url)
     if response.status_code == 200:
         html_content = response.text
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        # Objetivo 1: Obtener los nombres de los módulos en la página
-        modules = soup.find_all('h3', class_='wd-entities-title')
-        
-        for module in modules:
-            print(module.text.strip())
+        products = soup.find_all('div', class_='product-wrapper')
 
- 
-        
+        for product in products:
+            module = product.find('h3', class_='wd-entities-title')
+            price = product.find('span', class_='price')
+            stock = product.find('span', class_='out-of-stock product-label')
+
+            if module and price:  # Verifica tanto el módulo como el precio
+                print("Módulo:", module.text.strip())
+                print("Precio:", price.text.strip())
+
+                if stock:
+                    print("Stock:", stock.text.strip())
+                else:
+                    print("Stock: Disponible")
+                
+                print()
 
     else:
-                print("No se pudo acceder al sitio:", response.status_code)
+        print("No se pudo acceder al sitio:", response.status_code)
 
-#Segundo Proceso
+base_url = 'https://www.evophone.com.ar/categoria-producto/modulos/page/'
+page_numbers = range(1, 15)
+
+urls = [base_url + str(page) + '/' for page in page_numbers]
+
 for url in urls:
-    response = requests.get(url)
-    if response.status_code == 200:
-        html_content = response.text
-        soup = BeautifulSoup(html_content, 'html.parser')
-
-     
-        # Objetivo 2: Obtener los precios de los módulos en la página
-        prices = soup.find_all('span', class_='woocommerce-Price-amount amount')
-        print("\nPrecios:")
-        for price in prices:
-            print(price.text)
-        
-
-    else:
-                print("No se pudo acceder al sitio:", response.status_code)
+    process_page(url)
